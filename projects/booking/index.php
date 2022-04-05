82d808481660df74253502a2a40bc4768c46b5e8
Renzega <55640170+Renzega@users.noreply.github.com>
2022-04-05 19:23:51 +0200

    DEV

--- projects/booking/index.php
+++ projects/booking/index.php
@@ -3,20 +3,30 @@
 // On initialise la liste des pages accessibles depuis l'URL
 $pages = array('accueil', 'erreur404');
 
-// On vérifie si p est renseignée dans l'URL, qu'elle est bien une chaîne de caractères
-// et qu'elle est bien listée dans les pages autorisées (cf. $pages)
-if(isset($_GET['p']) && is_string($_GET['p']) && in_array($_GET['p'], $pages)) {
-    
+// On vérifie si p est renseignée dans l'URL
+if(isset($_GET['p'])) {
+
     // On récupère la page courante à l'aide de la variable d'URL "p", que l'on sécurise
     $currentPage = htmlentities(trim($_GET['p']));
+
+    // On vérifie que p est bien une chaîne de caractères
+    // et qu'elle est bien listée dans les pages autorisées (cf. $pages)
+    if(is_string($currentPage) && in_array($currentPage, $pages)) {
     
-    // On introduit le contrôleur lié à page en question
-    require('app/controllers/'.$currentPage.'.php');
+        // On introduit le contrôleur lié à page en question
+        require('app/controllers/'.$currentPage.'.php');
 
-} else { // Au moins une des conditions ci-dessus n'est pas respectée
+    } else {
 
-    // On redirige l'utilisateur vers la page d'erreur 404, en précisant le nom de la page recherchée
-    header('Location: index.php?p=erreur404&page='.$currentPage);
+        // On redirige l'utilisateur vers la page d'erreur 404, en précisant le nom de la page recherchée
+        header('Location: index.php?p=erreur404&page='.$currentPage);
+
+    }
+
+} else { 
+
+    // On redirige l'utilisateur vers la page d'accueil
+    header('Location: index.php?p=accueil');
 
 }
 
