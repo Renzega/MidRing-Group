963af4712d97c846ec4795174fe7ca326fef80b5
Renzega <55640170+Renzega@users.noreply.github.com>
2022-04-07 23:47:54 +0200

    Update main.dart

--- learn/flutter_project_1/lib/main.dart
+++ learn/flutter_project_1/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My First App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: const HomePage(title: 'Ma première application')
+    return CupertinoApp(
+      home: HomePage(title: 'Ma première application'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -29,112 +26,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Icon _likeHeart = const Icon(Icons.favorite_border, color: Colors.white);
-  bool _likeBool = false;
-
-  String _affichage = "0: Accueil";
-  int _indexSelectionne = 0;
-
-  void _likeThis() {
-    setState(() {
-      if(_likeBool) {
-        _likeHeart = const Icon(Icons.favorite_border, color: Colors.white);
-        _likeBool = false;
-      } else {
-        _likeHeart = const Icon(Icons.favorite, color: Colors.white);
-        _likeBool = true;
-      }
-    });
-  }
-
-  void _itemClique(int index) {
-    setState(() {
-      _indexSelectionne = index;
-      switch(_indexSelectionne) {
-        case 0:
-          {
-            _affichage = '$_indexSelectionne: Accueil';
-          }
-          break;
-        case 1:
-          {
-            _affichage = '$_indexSelectionne: Mon compte';
-          }
-          break;
-        case 2:
-          {
-            _affichage = '$_indexSelectionne: Statistiques';
-          }
-          break;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton (
-            icon: _likeHeart,
-            onPressed: _likeThis
-          )
-        ],
-        backgroundColor: Colors.red,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _likeThis,
-        backgroundColor: Colors.red,
-        child: _likeHeart
-      ),
-      body: Center(
+    return CupertinoTabScaffold(
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('${_affichage}', style: TextStyle(color: Colors.red, fontSize: 40.0))
-          ],
-        )
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            DrawerHeader(
-              child: Text(
-                'Menu de navigation',
-                style: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25
-                ),
-              ),
-            ),
-            Text('Lien 1'),
-            Text('Lien 2'),
-            Text('Lien 3'),
-          ],
+
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.white),
-            label: 'Accueil'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.supervised_user_circle, color: Colors.white),
-              label: 'Mon compte'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.assessment, color: Colors.white),
-              label: 'Statistiques'
-          ),
-        ],
-        backgroundColor: Colors.red,
-        onTap: _itemClique,
-        currentIndex: _indexSelectionne
       ),
     );
   }
