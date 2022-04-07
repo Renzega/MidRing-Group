e3bdc293044d874d8282a03401e2094e95a20253
Renzega <55640170+Renzega@users.noreply.github.com>
2022-04-07 17:46:29 +0200

    Update main.dart

--- learn/flutter_project_1/lib/main.dart
+++ learn/flutter_project_1/lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'code.dart';
-import 'future.dart';
 
 void main() {
-  OrdinateurFnac ordiFnac = OrdinateurFnac('Dell', 'AlienWare', 'RTX 3090', 'Intel Core i7', '32GB', 2000);
-  OrdinateurDarty ordiDarty = OrdinateurDarty(6, 'HP Omen', 'GTX 1060', 'Intel Core i7', '16GB', 1490);
-  Acheteur<OrdinateurFnac> acheteur1 = Acheteur('Samuel', 'Clauzon', 123, ordiFnac);
-  print(acheteur1.getOrdinateur());
   runApp(const MyApp());
 }
 
@@ -16,15 +10,132 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to My App')
-        ),
-        body: const Center(
-          child: Text('Content')
+      title: 'My First App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: const HomePage(title: 'Ma première application')
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Icon _likeHeart = const Icon(Icons.favorite_border, color: Colors.white);
+  bool _likeBool = false;
+
+  String _affichage = "0: Accueil";
+  int _indexSelectionne = 0;
+
+  void _likeThis() {
+    setState(() {
+      if(_likeBool) {
+        _likeHeart = const Icon(Icons.favorite_border, color: Colors.white);
+        _likeBool = false;
+      } else {
+        _likeHeart = const Icon(Icons.favorite, color: Colors.white);
+        _likeBool = true;
+      }
+    });
+  }
+
+  void _itemClique(int index) {
+    setState(() {
+      _indexSelectionne = index;
+      switch(_indexSelectionne) {
+        case 0:
+          {
+            _affichage = '$_indexSelectionne: Accueil';
+          }
+          break;
+        case 1:
+          {
+            _affichage = '$_indexSelectionne: Mon compte';
+          }
+          break;
+        case 2:
+          {
+            _affichage = '$_indexSelectionne: Statistiques';
+          }
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton (
+            icon: _likeHeart,
+            onPressed: _likeThis
+          )
+        ],
+        backgroundColor: Colors.red,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _likeThis,
+        backgroundColor: Colors.red,
+        child: _likeHeart
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('${_affichage}', style: TextStyle(color: Colors.red, fontSize: 40.0))
+          ],
         )
-      )
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Menu de navigation',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25
+                ),
+              ),
+            ),
+            Text('Lien 1'),
+            Text('Lien 2'),
+            Text('Lien 3'),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.white),
+            label: 'Accueil'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle, color: Colors.white),
+              label: 'Mon compte'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.assessment, color: Colors.white),
+              label: 'Statistiques'
+          ),
+        ],
+        backgroundColor: Colors.red,
+        onTap: _itemClique,
+        currentIndex: _indexSelectionne
+      ),
     );
   }
 }
